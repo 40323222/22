715e07362459a7fa886bb83ec0ee7953b08e9e32
40323222 <40323222@gm.nfu.edu.tw>
2014-12-15 21:11:43 +0800

    a

---
+++ setup.py
@@ -0,0 +1,18 @@
+#@+leo-ver=5-thin
+#@+node:2015.20141215193132.1776: * @file V:\happy\setup.py
+#@@language python
+#@@tabwidth -4
+#@+others
+#@+node:2015.20141215193132.1777: ** setup declarations
+from setuptools import setup
+
+setup(name='YourAppName',
+      version='1.0',
+      description='OpenShift App',
+      author='Your Name',
+      author_email='example@example.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Cherrypy'],
+     )
+#@-others
+#@-leo
